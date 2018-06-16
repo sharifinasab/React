4782f157e8270b842a422763494894c29d3018a1
Ali <ali.s@myplanet.com>
2018-06-16 14:53:17 +0430

    Adding Redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,47 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import { createStore } from 'redux';
+
+const defaultState = { checked: false };
+const reducer = function(state = defaultState, action) {
+  switch (action.type) {
+    case 'TOGGLE':
+      return { ...state, checked: !state.checked };
+  }
+  return state;
+};
+const store = createStore(reducer);
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  componentWillMount() {
+    store.subscribe(() => this.setState(store.getState()));
+    // subscription takes place after Redux store initialization action
+    // so listener won't update component's initial state -
+    // the app state needs to be synchronized manually
+    this.setState(store.getState());
+  }
+
   render() {
+    const onClick = () => store.dispatch({ type: 'TOGGLE' });
     return (
-      <h1>Hello, World!</h1>
+      <div>
+        <h1>To-dos</h1>
+        <div>
+          Learn Redux&nbsp;
+          <input
+            type="checkbox"
+            checked={!!this.state.checked}
+            onClick={onClick} />
+        </div>
+        {
+          this.state.checked ? (<h2>Done!</h2>) : null
+        }
+      </div>
     );
   }
 }
